e99e2cc8df4757b93155c37be829d2d9be904f1a
Isaac <isaacforschool05@gmail.com>
2023-03-21 14:35:43 +0800

    successfully fetched users

--- src/Pages/Cocktails.js
+++ src/Pages/Cocktails.js
@@ -7,20 +7,30 @@ import BASE_API from "../Components/BASE_API";
 
 export default class Cocktails extends React.Component {
     state = {
-        posts: []
+        posts: [],
+        users: []
 
     }
+     
 
     loadPosts = async () => {
         const response = await axios.get(`${BASE_API}cocktails`);
         this.setState({
             posts: response.data
-        }, () => console.log(this.state.posts));
+        }, () => console.log("Posts:",this.state.posts));
+    }
+
+    loadUsers = async () => {
+        const response = await axios.get(`${BASE_API}users`);
+        this.setState({
+            users: response.data
+        }, () => console.log("Users:", this.state.users));
     }
 
     componentDidMount = () => {
         try {
             this.loadPosts();
+            this.loadUsers()
         } catch (e) {
             console.log("Error fetching data:", e.message)
         }
@@ -33,7 +43,8 @@ export default class Cocktails extends React.Component {
                         {this.state.posts.map(post => (
                             <div className="col-sm-12 col-md-6 col-lg-4 mt-3" key={post._id}>
                                 <CocktailCard name={post.name}
-                                              imageUrl={post.imageUrl}  />
+                                              imageUrl={post.imageUrl}
+                                              user={this.state.users}  />
                             </div>
                         ))}
                 </div>
