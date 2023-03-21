bc8fbb722bb6417348dd7d7f6691fc86495eec9e
Isaac <isaacforschool05@gmail.com>
2023-03-21 16:44:01 +0800

    create form working, functionalities not yet added

--- src/Components/NewCocktail.js
+++ src/Components/NewCocktail.js
@@ -2,12 +2,26 @@ import React from "react";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import { Row, Col } from "react-bootstrap";
 
 export default function NewCocktail(props) {
     return (<div>
-        <Modal>
-            <Modal.Title>Add a new cocktail</Modal.Title>
-
+        <Modal show={props.formStatus}>
+            <Modal.Header>
+                <Modal.Title>Add a new cocktail</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Row>
+                        <Col>
+                            <Form.Control placeholder="First name" />
+                        </Col>
+                        <Col>
+                            <Form.Control placeholder="Last name" />
+                        </Col>
+                    </Row>
+                </Form>
+            </Modal.Body>
         </Modal>
     </div>)
 }
--- src/Pages/Cocktails.js
+++ src/Pages/Cocktails.js
@@ -4,22 +4,24 @@ import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import BASE_API from "../Components/BASE_API";
+import NewCocktail from "../Components/NewCocktail";
 
 export default class Cocktails extends React.Component {
     state = {
         posts: [],
         users: [],
         cocktailModal: false,
-        activeCocktailModal: ""
+        activeCocktailModal: "",
+        cocktailFormStatus: false
 
     }
-     
+
     // READ FUNCTIONS
     loadPosts = async () => {
         const response = await axios.get(`${BASE_API}cocktails`);
         this.setState({
             posts: response.data
-        }, () => console.log("Posts:",this.state.posts));
+        }, () => console.log("Posts:", this.state.posts));
     }
 
     loadUsers = async () => {
@@ -43,6 +45,12 @@ export default class Cocktails extends React.Component {
         })
     }
 
+    toggleCocktailForm = () => {
+        this.setState({
+            cocktailFormStatus: "true"
+        });
+    }
+
     componentDidMount = () => {
         try {
             this.loadPosts();
@@ -54,23 +62,27 @@ export default class Cocktails extends React.Component {
 
     render() {
         return (<div>
-            <button className="mt-3 mx-auto">Add New Cocktail</button>
+
             <div className="container">
                 <div className="row">
+                    <button className="mt-3 mx-auto" onClick={this.toggleCocktailForm}>Add New Cocktail</button>
+                    <NewCocktail formStatus={this.state.cocktailFormStatus} />
+                    <div className="row">
                         {this.state.posts.map(post => (
                             <div className="col-sm-12 col-md-6 col-lg-4 mt-3" key={post._id}>
                                 <CocktailCard name={post.name}
-                                              imageUrl={post.imageUrl}
-                                              user={this.state.users.find(user => user._id === post.userId)}  
-                                              cocktailModalStatus={this.state.cocktailModal}
-                                              cocktailModalId={this.state.activeCocktailModal}
-                                              toggleCocktailModal={() => this.toggleCocktailModal(post._id)}
-                                              closeCocktailModal={this.closeCocktailModal}/>
-                                            
+                                    imageUrl={post.imageUrl}
+                                    user={this.state.users.find(user => user._id === post.userId)}
+                                    cocktailModalStatus={this.state.cocktailModal}
+                                    cocktailModalId={this.state.activeCocktailModal}
+                                    toggleCocktailModal={() => this.toggleCocktailModal(post._id)}
+                                    closeCocktailModal={this.closeCocktailModal} />
+
                             </div>
                         ))}
+                    </div>
                 </div>
             </div>
-            </div>)
+        </div>)
     }
 }
