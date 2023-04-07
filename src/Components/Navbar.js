149d9ffb88e54509c028caf975ca4b332c432778
Isaac <isaacforschool05@gmail.com>
2023-04-07 16:24:36 +0800

    removed hrefs

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,7 +9,7 @@ export default function () {
     return (
         <nav className=" navbar navbar-expand-lg px-0 py-3 navbar-color mb-0">
             <div className="container-xl">
-                <a className="navbar-brand" href="#">
+                <a className="navbar-brand">
                     <img src="" className="h-8" alt="Cocktail" />
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,16 +17,16 @@ export default function () {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarCollapse">
                     <div className="navbar-nav mx-lg-auto">
-                        <a className="nav-item nav-link active" href="#" aria-current="page">Home</a>
-                        <a className="nav-item nav-link" href="#">Explore</a>
-                        <a className="nav-item nav-link" href="#">Blog</a>
-                        <a className="nav-item nav-link" href="#">Contact</a>
+                        <a className="nav-item nav-link active"  aria-current="page">Home</a>
+                        <a className="nav-item nav-link">Explore</a>
+                        <a className="nav-item nav-link">Blog</a>
+                        <a className="nav-item nav-link">Contact</a>
                     </div>
                     <div className="navbar-nav ms-lg-4">
-                        <a className="nav-item nav-link" href="#">Sign in</a>
+                        <a className="nav-item nav-link">Sign in</a>
                     </div>
                     <div className="d-flex align-items-lg-center mt-3 mt-lg-0">
-                        <a href="#" className="btn btn-sm btn-primary w-full w-lg-auto">
+                        <a className="btn btn-sm btn-primary w-full w-lg-auto">
                             Register
                         </a>
                     </div>
