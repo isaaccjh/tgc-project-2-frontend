48c88cb0787342c6705ee695f5ee3e033fff39d9
Isaac <isaacforschool05@gmail.com>
2023-04-09 23:31:05 +0800

    removed the href

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,7 +9,7 @@ export default function Navbar () {
     return (
         <nav className=" navbar navbar-expand-lg px-0 py-3 navbar-color mb-0">
             <div className="container-xl">
-                <a className="navbar-brand" href="l">
+                <a className="navbar-brand" href="#">
                     <img src={require("../Sparkle.png")} id="image" style={{"height":"32px", "width":"32px", "scale": "3.5"}} className="h-8" alt="Cocktail" />
                 </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,16 +17,16 @@ export default function Navbar () {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarCollapse">
                     <div className="navbar-nav mx-lg-auto">
-                        <a className="nav-item nav-link active" href="l" aria-current="page">Home</a>
-                        <a className="nav-item nav-link" href="l">Explore</a>
-                        <a className="nav-item nav-link" href="l">Blog</a>
-                        <a className="nav-item nav-link" href="l">Contact</a>
+                        <a className="nav-item nav-link active" href="#" aria-current="page">Home</a>
+                        <a className="nav-item nav-link" href="#">Explore</a>
+                        <a className="nav-item nav-link" href="#">Blog</a>
+                        <a className="nav-item nav-link" href="#">Contact</a>
                     </div>
                     <div className="navbar-nav ms-lg-4">
-                        <a className="nav-item nav-link" href="l">Sign in</a>
+                        <a className="nav-item nav-link" href="#">Sign in</a>
                     </div>
                     <div className="d-flex align-items-lg-center mt-3 mt-lg-0">
-                        <a href="l" className="btn btn-sm btn-primary w-full w-lg-auto">
+                        <a href="#" className="btn btn-sm btn-primary w-full w-lg-auto">
                             Register
                         </a>
                     </div>
